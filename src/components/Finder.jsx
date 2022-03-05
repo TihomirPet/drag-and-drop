2460cebcbe3997fa70e319a5a539cfd4cc82a834
TihomirPet <tihomirpetrov@web.de>
2022-03-05 19:43:52 +0100

    add react graggble make data task draggble

--- src/components/Finder.jsx
+++ src/components/Finder.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
+import React from 'react';
 import BarChart from './chart/BarChart';
+import Drag from './chart/draggableelement/Drag';
 import DragListC from './dragAndDrop copy/DragListC';
-import DragList from './dragAndDrop/DragList'
-import TaskList from './task/TaskList'
-
-
+import DragList from './dragAndDrop/DragList';
+import TaskList from './task/TaskList';
 
 export default function Finder() {
   return (
     <>
-      <DragList />
-      {/* <TaskList /> */}
-   {/* <BarChart/> */}
+      {/* <DragList /> */}
+      <TaskList />
+      {/* <BarChart/> */}
 
-   {/* <DragListC/> */}
+      {/* <DragListC/> */}
+
+      {/* <Drag /> */}
     </>
   );
 }
--- src/components/task/TaskItem.jsx
+++ src/components/task/TaskItem.jsx
@@ -1,26 +1,34 @@
 import React from 'react'
-
+import Draggable from 'react-draggable';
 export default function TaskItem({list}) {
   return (
     <div>
-      {list.map((listitem) => (
-        <div
-          className='border border-danger container
-        mt-5 p-2'>
-          <div className='border col-5 p-2 mt-2'>
-            assigneeName {listitem.assigneeName}
+      {list.map((listitem) => {
+        return (
+          <div className='container-fluid border border-primary p-2'>
+            <Draggable>
+              <div className='border col-5 p-2 mt-2'>
+                assigneeName {listitem.assigneeName}
+              </div>
+            </Draggable>
+            <Draggable>
+              <div className='border col-5 p-2 mt-2'>
+                completed_at {listitem.completed_at}
+              </div>
+            </Draggable>
+            <Draggable>
+              <div className='border col-5 p-2 mt-2'>
+                completed_calendarweek {listitem.completed_calendarweek}
+              </div>
+            </Draggable>
+            <Draggable>
+              <div className='border col-5 p-2 mt-2'>
+                completed_date {listitem.completed_date}
+              </div>
+            </Draggable>
           </div>
-          <div className='border col-5 p-2 mt-2'>
-            completed_at {listitem.completed_at}
-          </div>
-          <div className='border col-5 p-2 mt-2'>
-            completed_calendarweek {listitem.completed_calendarweek}
-          </div>
-          <div className='border col-5 p-2 mt-2'>
-            completed_date {listitem.completed_date}
-          </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 }
